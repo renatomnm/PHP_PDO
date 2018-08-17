a83cc9d57c2fd75f6b1ff32c63cbcb357833c274
unknown <andres@digitalhouse.com>
2018-08-16 21:17:28 -0300

    fetchAll

---
+++ Aula01/query.php
@@ -0,0 +1,11 @@
+<?php
+  include("connection.php");
+
+  $query = $conn->query("SELECT * FROM movies LIMIT 10");
+
+  $resulta = $query->fetchAll();
+  echo "<pre>";
+  print_r($resulta);
+  echo "</pre>";
+
+?>
