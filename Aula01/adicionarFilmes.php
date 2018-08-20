c372afecf0bcbc4a6d62910777d1e522322c3d33
unknown <andres@digitalhouse.com>
2018-08-20 20:12:51 -0300

    Adicionando filme da base de dados via PDO

--- Aula01/adicionarFilmes.php
+++ Aula01/adicionarFilmes.php
@@ -1,4 +1,24 @@
 <?php
 include("connection.php");
 
+$title = $_POST['title'];
+$rating = $_POST['rating'];
+$awards = $_POST['awards'];
+$release_date = $_POST['release_date'];
+$length = $_POST['length'];
+$genre_id = $_POST['genre_id'];
+
+$statement = $conn->prepare('INSERT INTO movies(title,rating,awards,release_date,length,genre_id) VALUES(:title, :rating, :awards, :release_date, :length, :genre_id)');
+
+$statement->execute([
+  ':title'=>$title,
+  ':rating'=>$rating,
+  ':awards'=>$awards,
+  ':release_date'=>$release_date,
+  ':length'=>$length,
+  ':genre_id'=>$genre_id
+]);
+
+
+echo "registro adicionado com sucesso";
 ?>
--- Aula01/filmes.php
+++ Aula01/filmes.php
@@ -12,6 +12,27 @@
 
 <ul>
   <?php  while($row = $q->fetch(PDO::FETCH_OBJ)) {?>
-  <li><?php echo $row->name; ?></li>
+  <li><?php echo $row->id . " - " . $row->name;
+    ?>
+  </li>
 <?php } ?>
 </ul>
+
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title></title>
+  </head>
+  <body>
+    <form class="adicionarfilmes" action="adicionarFilmes.php" method="post">
+        <input type="text" name="title" value="title">
+        <input type="text" name="rating" value="rating">
+        <input type="text" name="awards" value="awards">
+        <input type="text" name="release_date" value="release_date">
+        <input type="text" name="length" value="length">
+        <input type="text" name="genre_id" value="genre_id">
+        <input type="submit" name="enviar" value="enviar">
+    </form>
+  </body>
+</html>
