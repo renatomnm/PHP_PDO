82a3406d0597fd7f18d3505d66257223cba3660b
unknown <andres@digitalhouse.com>
2018-08-20 20:58:05 -0300

    Exercicio 2, doidera

--- Aula01/adicionarFilmes.php
+++ Aula01/adicionarFilmes.php
@@ -1,6 +1,7 @@
 <?php
 include("connection.php");
 
+
 $title = $_POST['title'];
 $rating = $_POST['rating'];
 $awards = $_POST['awards'];
@@ -8,17 +9,40 @@ $release_date = $_POST['release_date'];
 $length = $_POST['length'];
 $genre_id = $_POST['genre_id'];
 
-$statement = $conn->prepare('INSERT INTO movies(title,rating,awards,release_date,length,genre_id) VALUES(:title, :rating, :awards, :release_date, :length, :genre_id)');
+$verify = $conn->prepare('SELECT * FROM movies WHERE title = :title');
+$verify->execute([':title'=>$title]);
+$result = [];
+  while($row = $verify->fetch(PDO::FETCH_OBJ)){
+    $result[] = $row->title;
+  }
+if(!empty($result)){
 
-$statement->execute([
-  ':title'=>$title,
-  ':rating'=>$rating,
-  ':awards'=>$awards,
-  ':release_date'=>$release_date,
-  ':length'=>$length,
-  ':genre_id'=>$genre_id
-]);
+  $setAwards = $conn->prepare('UPDATE movies SET awards = :awards');
+  $verify->prepare('UPDATE movies SET release_date = :release_date');
+  $verify->prepare('UPDATE movies SET length = :length');
+  $verify->prepare('UPDATE movies SET genre_id = :genre_id');
+  $setAwards->execute([
+    ':rating' =>$rating,
+    ':awards' =>$awards,
+    ':release_date'=>$release_date,
+    ':length'=>$length,
+    ':genre_id'=>$genre_id
+  ]);
+  echo "registro atualizado com sucesso";
+}
+else{
+  $statement = $conn->prepare('INSERT INTO movies(title,rating,awards,release_date,length,genre_id) VALUES(:title, :rating, :awards, :release_date, :length, :genre_id)');
 
+  $statement->execute([
+    ':title'=>$title,
+    ':rating'=>$rating,
+    ':awards'=>$awards,
+    ':release_date'=>$release_date,
+    ':length'=>$length,
+    ':genre_id'=>$genre_id
+  ]);
 
-echo "registro adicionado com sucesso";
+  echo "registro adicionado com sucesso";
+}
+
 ?>
